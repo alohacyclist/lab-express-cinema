23015b77ab0ab460081e7729fac22f0122751227
[Christian Mueller] <[c-mueller87@live.de]>
2022-01-29 08:24:52 +0100

    done without styling

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get('/', (req, res) => res.render('index'));
+
+router.get('/movies', async (req, res) => {
+    try {
+        const allMovies = await Movie.find()
+        res.render('movies', {allMovies})
+    } catch (err) {
+        console.log('Something went wrong!',err)
+    }
+})
+
+router.get('/movie/:id', async (req, res) => {
+    try { 
+        const movieId = req.params.id
+        const thisMovie = await Movie.findById(movieId)
+        res.render('movie', {thisMovie})
+    } catch (err) {
+        console.log('Something went wrong!',err)
+    }
+})
 
 module.exports = router;
